d1b1f54a209fcac4b66c4fb7b0c782014229a93f
joligario448 <102866925+joligario448@users.noreply.github.com>
2022-08-11 09:53:45 -0400

    Add webapp-mysql deploy-delete app to scripts folder

---
+++ scripts/del_SimpleWebappMysql.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+echo "\n------------------------------------"
+echo "*** Removing Simple Webapp MySQL ..."
+echo "------------------------------------\n"
+
+kubectl delete -f ../secret-data.yaml -n final
+kubectl delete -f ../mysql-deployment-definition.yaml -n final
+kubectl delete -f ../mysql-service.yaml -n final
+kubectl delete -f ../webapp-configmap.yaml -n final
+kubectl delete -f ../webapp-deployment-definition.yaml -n final
+kubectl delete -f ../mysql-pvc.yaml -n final
+kubectl delete -f ../service-account-definition.yaml -n final
+kubectl delete -f ../serviceaccount-role.yaml -n final
+kubectl delete -f ../serviceaccount-rolebinding.yaml -n final
+kubectl delete -f ../webapp-service.yaml -n final
+# kubectl delete -f ../webapp-service-np.yaml -n final
+# kubectl delete -f ../mysql-pod-definition.yaml -n final
+# kubectl delete -f ../webapp-pod-definition.yaml -n final
+sleep 3
+
+echo "\n------------------------------------------"
+echo "*** All services removed successfully. ***"
+echo "------------------------------------------\n"
+kubectl get all -n final
+
